8a548b319d233b7f99dc246e30d04b7c88263dc6
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-11-23 16:14:43 -0700

    adds blogposts context to index screen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,9 +3,16 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    {title: 'First blog'},
+    {title: '2nd coming'}
+  ];
+
   return (
-    <BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>
       {children}
     </BlogContext.Provider>
   )
 };
+
+export default BlogContext;
