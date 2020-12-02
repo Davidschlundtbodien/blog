022cf0e61cc6c129dd1975a95c68b6ea2d6d793b
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-02 14:46:02 -0700

    adds text input to create screen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, {useContent, useState} from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput 
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+       />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input} 
+        value={content} 
+        onChangeText={(text) => setContent(text)} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10
+  }
+});
 
 export default CreateScreen;
