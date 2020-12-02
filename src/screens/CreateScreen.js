f0119a8488f66f9c71df3c0568e7ffa6d00cb997
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-02 16:10:02 -0700

    adds blog post save and transistion to index

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,12 @@
-import React, {useContent, useState} from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, {useContext, useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
-const CreateScreen = () => {
+const CreateScreen = ({navigation}) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const { addBlogPost } = useContext(Context);
+
   return (
     <View>
       <Text style={styles.label}>Enter Title:</Text>
@@ -18,6 +20,14 @@ const CreateScreen = () => {
         style={styles.input} 
         value={content} 
         onChangeText={(text) => setContent(text)} />
+      <Button 
+        title="Add Blog Post"
+        onPress={() => {
+          addBlogPost(title, content, () => {
+            navigation.navigate('Index');
+          });
+        }}
+      />
     </View>
   );
 };
@@ -26,11 +36,15 @@ const styles = StyleSheet.create({
   input: {
     fontSize: 18,
     borderWidth: 1,
-    borderColor: 'black'
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
   },
   label: {
     fontSize: 20,
-    marginBottom: 10
+    marginBottom: 5,
+    marginLeft: 5
   }
 });
 
