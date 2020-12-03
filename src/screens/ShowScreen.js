afd0372f43f94a17db0c71ebdb14470187f59f6b
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-03 15:51:06 -0700

    adds text input to edit screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext }from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
-import { EvilIcons} from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({navigation}) => {
   const { state } = useContext(Context);
@@ -18,8 +18,8 @@ const ShowScreen = ({navigation}) => {
 ShowScreen.navigationOptions = ({navigation}) => {
   return {
     headerRight: () => (
-      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
-        <EvilIcons name="pencil" size={30} />
+      <TouchableOpacity onPress={() => navigation.navigate('Edit', {id: navigation.getParam('id')})}>
+        <EvilIcons name="pencil" size={35} />
       </TouchableOpacity>
     )
   };
