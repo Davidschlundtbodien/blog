cd035aa145600257c5fe2863143829f54c9c919b
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-04 10:57:58 -0700

    adds BlogPostForm to Create Screen

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
@@ -20,8 +20,7 @@ const BlogPostForm = () => {
         onChangeText={(text) => setContent(text)} />
       <Button
         title="Save Blog Post"
-        onPress={() => {
-        }}
+        onPress={() => onSubmit(title, content)}
       />
     </View>
   );
